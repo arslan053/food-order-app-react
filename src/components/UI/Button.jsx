e374eef3ba18c4736a514a660c23b64f99972a6c
Muhammad Arslan <arslan05342@gmail.com>
2024-10-12 12:00:23 +0500

    Basic components are setup and data is being shown on screen

---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,8 @@
+export default function Button({ textOnly, className, children, ...props}){
+  let cssClasses = textOnly ? 'text-button' : 'button'; 
+  cssClasses += ' ' + className;
+
+  return <button className= {cssClasses} {...props}>
+    {children}
+  </button>
+}
